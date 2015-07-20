57a02cb43f654f136cbfe6b1f3eb73918c6b2d27
PiWalker <ldspcjunkie@gmail.com>
2015-07-20 17:01:36 -0600

    testing things

--- src/main/java/com/piwalker/letsmodreboot/referance/Textures.java
+++ src/main/java/com/piwalker/letsmodreboot/referance/Textures.java
@@ -4,5 +4,5 @@ package com.piwalker.letsmodreboot.referance;
  * Created by SamuelPiWalker on 7/20/2015.
  */
 public class Textures {
-    public static final String RESOURCE_PREFIX = Referance.MOD_ID.toLowerCase() + ":";
+    public static final String RESOURCE_PREFIX = Referance.MOD_ID + ":";
 }
