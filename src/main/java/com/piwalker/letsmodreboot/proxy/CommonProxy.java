979f0685cc7fce3200d4c3187193be58c7789498
PiWalker <ldspcjunkie@gmail.com>
2015-07-20 17:14:41 -0600

    testing things

--- src/main/java/com/piwalker/letsmodreboot/proxy/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package com.piwalker.letsmodreboot.proxy;
-
-/**
- * Created by SamuelPiWalker on 7/19/2015.
- */
-public abstract class CommonProxy implements  IProxy {
-
-
-}
--- src/main/java/com/piwalker/letsmodreboot/proxy/IProxy.java
+++
@@ -1,7 +0,0 @@
-package com.piwalker.letsmodreboot.proxy;
-
-/**
- * Created by SamuelPiWalker on 7/19/2015.
- */
-public interface IProxy {
-}
